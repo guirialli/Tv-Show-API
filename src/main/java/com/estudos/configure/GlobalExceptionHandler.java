c3bc7410140fb93d6fad5a849c27f9b94ae13704
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2024-08-04 17:56:18 -0300

    ref: ImbdController

--- src/main/java/com/estudos/configure/GlobalExceptionHandler.java
+++ src/main/java/com/estudos/configure/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.estudos.configure;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,19 +12,26 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
+
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<ResponseError> handleMissingServletRequestParameterException(
+			MissingServletRequestParameterException ex) {
+		var err = new ResponseError(400, ex.getMessage());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
+	}
+
 	@ExceptionHandler(JsonMappingException.class)
 	public ResponseEntity<ResponseError> handleJsonMappingException(JsonMappingException ex) {
-		var err  = new ResponseError(500, "Internal Error");
+		var err = new ResponseError(500, "Internal Error");
 		System.err.println(ex.getMessage());
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
 	}
-	
+
 	@ExceptionHandler(JsonProcessingException.class)
 	public ResponseEntity<ResponseError> handleJsonProcessingException(JsonProcessingException ex) {
-		var err  = new ResponseError(500, "Internal Error");
+		var err = new ResponseError(500, "Internal Error");
 		System.err.println(ex.getMessage());
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(err);
 	}
-	
+
 }
--- src/main/java/com/estudos/provider/series/imdb/ImdbController.java
+++ src/main/java/com/estudos/provider/series/imdb/ImdbController.java
@@ -1,14 +1,12 @@
 package com.estudos.provider.series.imdb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.estudos.common.dto.ResponseError;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
@@ -20,21 +18,16 @@ public class ImdbController {
 	private ImdbService service;
 
 	@GetMapping
-	public ResponseEntity<Object> getTvShow(@RequestParam(required = false) String title,
-			@RequestParam(required = false) String season, @RequestParam(required = false) String episode)
-			throws JsonMappingException, JsonProcessingException {
-		if (title != null && season != null && episode != null) {
-			var episodeData = this.service.findByEpisode(title, season, episode);
-			return ResponseEntity.ok(episodeData);
-		} else if (title != null && season != null) {
-			var episodeData = this.service.findBySeason(title, season);
-			return ResponseEntity.ok(episodeData);
+	public ResponseEntity<Object> getTvShow(@RequestParam() String title, @RequestParam(required = false) String season,
+			@RequestParam(required = false) String episode) throws JsonMappingException, JsonProcessingException {
+		Object response;
+		if (season != null && episode != null)
+			response = this.service.findByEpisode(title, season, episode);
+		else if (season != null)
+			response = this.service.findBySeason(title, season);
+		else
+			response = this.service.findTvShowByTitle(title);
+		return ResponseEntity.ok(response);
 
-		} else if (title != null) {
-			var tvShow = this.service.findTvShowByTitle(title);
-			return ResponseEntity.ok(tvShow);
-		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(new ResponseError(400, "You need insert a param like: title, season, episode!"));
 	}
 }
