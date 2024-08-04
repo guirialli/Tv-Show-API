3c0209d5f67d7120ac7c9fd1307b4d44d5310dd5
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2024-08-04 17:44:16 -0300

    fix: change _ -> + in ConvertPara.parser

--- src/main/java/com/estudos/common/convert/ConvertParam.java
+++ src/main/java/com/estudos/common/convert/ConvertParam.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class ConvertParam {
 	
 	public String parser(String param) {
-		return param.replaceAll(" ", "_").toLowerCase();
+		return param.replaceAll(" ", "+").toLowerCase();
 	}
 }
