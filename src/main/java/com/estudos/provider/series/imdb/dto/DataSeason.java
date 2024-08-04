525cce9284a4a694a2986cd8e6fd9ec9b9f8e880
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2024-08-04 17:39:39 -0300

    fix: change Array  -> List in DataSeason

--- src/main/java/com/estudos/provider/series/imdb/dto/DataSeason.java
+++ src/main/java/com/estudos/provider/series/imdb/dto/DataSeason.java
@@ -1,10 +1,12 @@
 package com.estudos.provider.series.imdb.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DataSeason(@JsonAlias("Title") String title, @JsonAlias("Season") String season,
-		@JsonAlias("totalSeasons") String totalSeasons, @JsonAlias("Episodes") DataEpisodes[] episodes) {
+		@JsonAlias("totalSeasons") String totalSeasons, @JsonAlias("Episodes") List<DataEpisodes> episodes) {
 
 }
